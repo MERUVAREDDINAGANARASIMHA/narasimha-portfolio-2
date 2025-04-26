b323d0714e03738c81a8612a37eeb9d509ebbab3
MERUVA REDDI NAGA NARASIMHA <92923702+MERUVAREDDINAGANARASIMHA@users.noreply.github.com>
2025-04-26 21:51:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const toggleBtn = document.getElementById('theme-toggle');
-
-toggleBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark-theme');
+const toggleMode = document.getElementById('toggleMode');
+toggleMode.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  if (document.body.classList.contains('dark-mode')) {
+    toggleMode.textContent = '☀️ Light Mode';
+  } else {
+    toggleMode.textContent = '🌙 Dark Mode';
+  }
 });
